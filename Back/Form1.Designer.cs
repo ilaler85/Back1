511be0a319f565b8781d04d524e0cd7ad9897763
ilaler85 <misha.galileo@yandex.ru>
2020-05-20 17:16:31 +0300

    Add files via upload

---
+++ Back/Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace Back
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.info = new System.Windows.Forms.ToolStripMenuItem();
+            this.run = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.riska = new System.Windows.Forms.TextBox();
+            this.riska2 = new System.Windows.Forms.TextBox();
+            this.probirka1 = new System.Windows.Forms.ProgressBar();
+            this.probirka2 = new System.Windows.Forms.ProgressBar();
+            this.probirka3 = new System.Windows.Forms.ProgressBar();
+            this.p1 = new System.Windows.Forms.Label();
+            this.p2 = new System.Windows.Forms.Label();
+            this.p3 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.info,
+            this.run});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(400, 28);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // info
+            // 
+            this.info.Name = "info";
+            this.info.Size = new System.Drawing.Size(60, 24);
+            this.info.Text = "Инфо";
+            this.info.Click += new System.EventHandler(this.info_Click);
+            // 
+            // run
+            // 
+            this.run.Name = "run";
+            this.run.Size = new System.Drawing.Size(46, 24);
+            this.run.Text = "Run";
+            this.run.Click += new System.EventHandler(this.runToolStripMenuItem_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(210, 34);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Введите цену деления шкалы \r\nв 1 пробирке";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(269, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(102, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "во 2 пробирке";
+            // 
+            // riska
+            // 
+            this.riska.Location = new System.Drawing.Point(34, 109);
+            this.riska.Name = "riska";
+            this.riska.Size = new System.Drawing.Size(100, 22);
+            this.riska.TabIndex = 6;
+            // 
+            // riska2
+            // 
+            this.riska2.Location = new System.Drawing.Point(272, 109);
+            this.riska2.Name = "riska2";
+            this.riska2.Size = new System.Drawing.Size(100, 22);
+            this.riska2.TabIndex = 7;
+            this.riska2.MouseMove += new System.Windows.Forms.MouseEventHandler(this.riska2_MouseMove);
+            // 
+            // probirka1
+            // 
+            this.probirka1.Location = new System.Drawing.Point(34, 171);
+            this.probirka1.Name = "probirka1";
+            this.probirka1.Size = new System.Drawing.Size(319, 23);
+            this.probirka1.TabIndex = 8;
+            this.probirka1.Value = 100;
+            this.probirka1.MouseEnter += new System.EventHandler(this.probirka1_MouseEnter);
+            // 
+            // probirka2
+            // 
+            this.probirka2.Location = new System.Drawing.Point(34, 241);
+            this.probirka2.Name = "probirka2";
+            this.probirka2.Size = new System.Drawing.Size(319, 23);
+            this.probirka2.TabIndex = 9;
+            this.probirka2.MouseEnter += new System.EventHandler(this.probirka2_MouseEnter);
+            // 
+            // probirka3
+            // 
+            this.probirka3.Location = new System.Drawing.Point(33, 306);
+            this.probirka3.Name = "probirka3";
+            this.probirka3.Size = new System.Drawing.Size(320, 23);
+            this.probirka3.TabIndex = 10;
+            this.probirka3.MouseEnter += new System.EventHandler(this.probirka3_MouseEnter);
+            // 
+            // p1
+            // 
+            this.p1.AutoSize = true;
+            this.p1.Location = new System.Drawing.Point(359, 177);
+            this.p1.Name = "p1";
+            this.p1.Size = new System.Drawing.Size(32, 17);
+            this.p1.TabIndex = 11;
+            this.p1.Text = "100";
+            // 
+            // p2
+            // 
+            this.p2.AutoSize = true;
+            this.p2.Location = new System.Drawing.Point(359, 247);
+            this.p2.Name = "p2";
+            this.p2.Size = new System.Drawing.Size(16, 17);
+            this.p2.TabIndex = 12;
+            this.p2.Text = "0";
+            // 
+            // p3
+            // 
+            this.p3.AutoSize = true;
+            this.p3.Location = new System.Drawing.Point(359, 315);
+            this.p3.Name = "p3";
+            this.p3.Size = new System.Drawing.Size(16, 17);
+            this.p3.TabIndex = 13;
+            this.p3.Text = "0";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(400, 341);
+            this.Controls.Add(this.p3);
+            this.Controls.Add(this.p2);
+            this.Controls.Add(this.p1);
+            this.Controls.Add(this.probirka3);
+            this.Controls.Add(this.probirka2);
+            this.Controls.Add(this.probirka1);
+            this.Controls.Add(this.riska2);
+            this.Controls.Add(this.riska);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "Main";
+            this.Text = "Мейн";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem info;
+        private System.Windows.Forms.ToolStripMenuItem run;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox riska;
+        private System.Windows.Forms.TextBox riska2;
+        private System.Windows.Forms.ProgressBar probirka1;
+        private System.Windows.Forms.ProgressBar probirka2;
+        private System.Windows.Forms.ProgressBar probirka3;
+        private System.Windows.Forms.Label p1;
+        private System.Windows.Forms.Label p2;
+        private System.Windows.Forms.Label p3;
+    }
+}
+
---
+++ Back/Form1.cs
@@ -0,0 +1,258 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Back
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+        ToolTip t = new ToolTip();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+ 
+        }
+        public async void pereliv1( int v1, int v2)
+        {
+            probirka1.Value = v1;
+            probirka3.Value = 100 - v1;
+            p1.Text = probirka1.Value.ToString();
+            p3.Text = probirka3.Value.ToString();
+            await Task.Delay(1000);
+            probirka2.Value = v2;
+            probirka1.Value = v1 - v2;
+            p1.Text = probirka1.Value.ToString();
+            p2.Text = probirka2.Value.ToString();
+            await Task.Delay(1000);
+            probirka2.Value  += probirka3.Value;
+            probirka3.Value = 0;
+            p2.Text = probirka2.Value.ToString();
+            p3.Text = probirka3.Value.ToString();
+            await Task.Delay(1000);
+            probirka1.Value--;
+            probirka3.Value = 1;
+            p1.Text = probirka1.Value.ToString();
+            p3.Text = probirka3.Value.ToString();
+        }
+
+        public async void pereliv2(int v1, int v2)
+        {
+            probirka2.Value = v2;
+            probirka3.Value = 100 - v2;
+            p2.Text = probirka2.Value.ToString();
+            p3.Text = probirka3.Value.ToString();
+            await Task.Delay(1000);
+            probirka1.Value = v1;
+            probirka2.Value = v2 - v1;
+            p1.Text = probirka1.Value.ToString();
+            p2.Text = probirka2.Value.ToString();
+            await Task.Delay(1000);
+            probirka1.Value += probirka3.Value;
+            probirka3.Value = 0;
+            p1.Text = probirka1.Value.ToString();
+            p3.Text = probirka3.Value.ToString();
+            await Task.Delay(1000);
+            probirka2.Value--;
+            probirka3.Value = 1;
+            p2.Text = probirka2.Value.ToString();
+            p3.Text = probirka3.Value.ToString();
+        }
+        public async void pereliv3(int v1, int v2)
+        {
+            probirka1.Value = 100-v2;
+            probirka2.Value = v2;
+            p1.Text = probirka1.Value.ToString();
+            p2.Text = probirka2.Value.ToString();
+            await Task.Delay(1000);
+            probirka3.Value = 1;
+            probirka1.Value--;
+            p1.Text = probirka1.Value.ToString();
+            p3.Text = probirka3.Value.ToString();
+        }
+        public async void pereliv4(int v1, int v2)
+        {
+            probirka1.Value = v1;
+            probirka2.Value = v2;
+            probirka3.Value = 100 - v1 - v2;
+            p1.Text = probirka1.Value.ToString();
+            p3.Text = probirka3.Value.ToString();
+            p2.Text = probirka2.Value.ToString();
+            await Task.Delay(1000);
+            probirka1.Value += v2;
+            probirka2.Value = 0;
+            p1.Text = probirka1.Value.ToString();
+            p2.Text = probirka2.Value.ToString();
+            await Task.Delay(1000);
+            probirka2.Value = probirka3.Value;
+            probirka3.Value = 0;
+            p2.Text = probirka2.Value.ToString();
+            p3.Text = probirka3.Value.ToString();
+            await Task.Delay(1000);
+            probirka1.Value --;
+            probirka3.Value = 1;
+            p1.Text = probirka1.Value.ToString();
+            p3.Text = probirka3.Value.ToString();
+        }
+        public async void pereliv5(int v1, int v2)
+        {
+            probirka1.Value = v1;
+            probirka2.Value = v2;
+            probirka3.Value = 100 - v1 - v2;
+            p1.Text = probirka1.Value.ToString();
+            p3.Text = probirka3.Value.ToString();
+            p2.Text = probirka2.Value.ToString();
+            await Task.Delay(1000);
+            Thread.Sleep(100);
+            probirka2.Value += v1;
+            probirka1.Value = 0;
+            p1.Text = probirka1.Value.ToString();
+            p2.Text = probirka2.Value.ToString();
+            await Task.Delay(1000);
+            Thread.Sleep(100);
+            probirka1.Value = probirka3.Value;
+            probirka3.Value = 0;
+            p1.Text = probirka1.Value.ToString();
+            p3.Text = probirka3.Value.ToString();
+            await Task.Delay(1000);
+            Thread.Sleep(100);
+            probirka2.Value--;
+            probirka3.Value = 1;
+            p3.Text = probirka3.Value.ToString();
+            p2.Text = probirka2.Value.ToString();
+        }
+
+        int ris1 = 1, ris2 = 1;
+        public bool check(int v1, int v2)
+        {
+            bool ook = false;
+            do
+            {
+                if (/*Math.Abs*/((v1 - v2) % ris1 == 1)&&(v1>v2))
+                {
+                    ook = true;
+                    pereliv1(v1, v2);
+                }
+                else
+                {
+                    if (/*Math.Abs*/((v1 - v2) % ris2 == 1)&&(v2>v1))
+                    {
+                        ook = true;
+                        pereliv2(v1, v2);
+                    }
+                    else
+                    {
+                        if (v1 + v2 == 99)
+                        {
+                            ook = true;
+                            pereliv3(v1,v2);
+                        }
+                        else
+                        {
+                            if (((v1 + v2) % ris1 == 1) && (v1 + v2 <= 100))
+                            {
+                                pereliv4(v1, v2);
+                                ook = true;
+                            }
+                            else
+                            {
+                                if(((v1 + v2) % ris2 == 1) && (v1 + v2 <= 100))
+                                {
+                                    pereliv5(v1, v2);
+                                    ook = true;
+                                }
+                                else
+                                {
+                                    v2 += ris2;     
+                                }
+                        
+                            }
+                        }
+                        /*if ((v1 + v2 == 99 || (v1 + v2) % ris1 == 1 || (v1 + v2) % ris2 == 1) && (v1 + v2 <= 100))
+                        {
+                            ook = true;
+                        }*/
+                        
+                    }
+                }
+            } while (v2 > 100 && (!ook));
+            if (v1 != 100 && !ook)
+            {
+                if (v1 + ris1 > 100)
+                {
+                    ook = check(100, 0);
+                }
+                else
+                {
+                    ook = check( v1 + ris1, 0);
+                }
+            }
+            
+            return ook;
+        }
+
+        public bool proverka(string tmp, ref int a)
+        {
+            return int.TryParse(tmp, out a);
+        }
+
+        private void runToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if(proverka(riska.Text,ref ris1)&& proverka(riska2.Text, ref ris2))
+            {
+
+                if (check(ris1, 0))
+                {
+                    MessageBox.Show("Можно перелить 1 мл в 3 пробирку");
+                }
+                else
+                {
+                    MessageBox.Show("Нельзя перелить 1 мл в 3 пробирку");
+                }
+            }
+            else
+            {
+                MessageBox.Show("У вас беды с целыми числами");
+            }
+        }
+
+        private void info_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("29. Задача о пробирках. Даны три пробирки объемом 100 мл. Две из них имеют шкалу рисок. Уровни рисок указываются пользователем. Из начального состояния { 100мл, 0мл, 0мл} (в первой пробирке 100мл, а две другие пусты) определить, возможен ли переход путем переливания жидкости из одной пробирки в другую в состояние { _, _, 1мл} (в пробирках с рисками— произвольное количество миллилитров, а в пробирке без рисок— 1мл).");
+        }
+
+        private void riska2_MouseMove(object sender, MouseEventArgs e)
+        {
+            
+        }
+
+        private void probirka1_MouseEnter(object sender, EventArgs e)
+        {
+            t.SetToolTip(probirka1, "я с рисками на " + riska.Text + " мл.");
+        }
+
+        private void probirka2_MouseEnter(object sender, EventArgs e)
+        {
+            t.SetToolTip(probirka2, "я с рисками на " + riska2.Text + " мл.");
+        }
+
+        private void probirka3_MouseEnter(object sender, EventArgs e)
+        {
+            t.SetToolTip(probirka3, "а я томат");
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
